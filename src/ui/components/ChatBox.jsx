2c71211305af63f7f8ffd1720ec99779125a7e8d
Oliver Diamond <oliver.diamond@tesglobal.com>
2018-04-29 18:48:19 +0100

    Better component

--- src/ui/components/ChatBox.jsx
+++ src/ui/components/ChatBox.jsx
@@ -11,35 +11,46 @@ class ChatBox extends React.Component {
   }
 
   componentDidMount () {
-    const socket = socketIO();
+    const socket = socketIO('ws://localhost:3000/');
     this.setState({ socket });
     socket.on('chat-id-assignment', chatId => this.setState({ chatId }));
     socket.on('chat-message', msg => this.setState({ messages: this.state.messages.concat(msg) }));
   }
 
-  sendMessage () {
-    const text = this.state.newMessage;
-    this.setState({ newMessage: '' });
-    this.state.socket.emit('chat-message', { text, sender: this.state.chatId });
-  }
-
   render () {
+    const Message = ({ text, sender }) => (
+      <div style={{ width: '100%' }}>
+        <span style={{ width: '20%' }}>
+          {sender === this.state.chatId ? 'Me: ' : `${sender.slice(-3)}: `}
+        </span>
+        <span style={{ width: '80%' }}>
+          {text}
+        </span>
+      </div>
+    );
+
     return (
       <div style={{ height: '300px', width: '300px', border: '1px solid #000' }}>
         <div style={{ height: '270px', width: '100%' }}>
-          { this.state.messages.map(({ sender, text }, id) => (
-            <div key={id} style={{ width: '100%' }}>
-              <span style={{ width: '20%' }}>{sender === this.state.chatId ? 'Me: ' : `${sender.slice(-3)}: `}</span>
-              <span style={{ width: '80%' }}>{text}</span>
-            </div>
-          )) }
+          { this.state.messages.map((message, i) => (<Message key={i} { ...message } />)) }
         </div>
         <div style={{ height: '30px', width: '100%' }}>
-          <input
-            style={{ width: '240px' }}
-            value={this.state.newMessage}
-            onChange={e => this.setState({ newMessage: e.target.value })}/>
-          <button onClick={() => this.sendMessage()}>Send</button>
+          <form>
+            <input
+              style={{ width: '240px' }}
+              value={this.state.newMessage}
+              onChange={e => this.setState({ newMessage: e.target.value })}/>
+            <button
+              onClick={e => {
+                  const text = this.state.newMessage;
+                  this.setState({ newMessage: '' });
+                  this.state.socket.emit('chat-message', { text, sender: this.state.chatId });
+                  e.preventDefault();
+              }}
+            >
+              Send
+            </button>
+          </form>
         </div>
       </div>
     );
